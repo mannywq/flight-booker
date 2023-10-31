7bbb48c5f4801242c4def18708d889cc712939a3
Manny <manny.wetterqvist@outlook.com>
2023-10-31 19:47:22 +0900

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,28 @@
 #
 puts 'Seeding database'
 Airport.destroy_all
+Flight.destroy_all
 
-a1 = Airport.create(code: 'FUK', name: 'Fukuoka')
-a2 = Airport.create(code: 'HND', name: 'Haneda')
-a3 = Airport.create(code: 'NRT', name: 'Narita')
-a4 = Airport.create(code: 'BNE', name: 'Brisbane')
-a5 = Airport.create(code: 'SIN', name: 'Singapore')
-a1.save
-a2.save
-a3.save
-a4.save
-a5.save
+airports = [
 
-f1 = Flight.new(flight_no: 'SQ655', departure_airport: a1, arrival_airport: a5,
-                duration: 410)
-f2 = Flight.new(flight_no: 'QF62', departure_airport: a2, arrival_airport: a4,
-                duration: 545)
-f1.save
-f2.save
+  { code: 'FUK', name: 'Fukuoka' },
+  { code: 'HND', name: 'Haneda' },
+  { code: 'NRT', name: 'Narita' },
+  { code: 'BNE', name: 'Brisbane' },
+  { code: 'SIN', name: 'Singapore' }
+]
+
+Airport.create(airports)
+
+flights = [
+  { flight_no: 'SQ655', departure_airport: Airport.find(1),
+    arrival_airport: Airport.find(5), duration: 410, departure_time: Time.now },
+  { flight_no: 'QF62', departure_airport: Airport.find(2),
+    arrival_airport: Airport.find(4), duration: 410, departure_time: Time.now },
+  { flight_no: 'NH1076', departure_airport: Airport.find(1),
+    arrival_airport: Airport.find(2), duration: 410, departure_time: Time.now }
+
+]
+Flight.create(flights)
 
 puts 'Seeding completed.'
