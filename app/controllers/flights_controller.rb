e4c908c6d31ca3924c7e89a1e92a983aaccc46cb
Manny <manny.wetterqvist@outlook.com>
2023-11-01 19:45:00 +0900

    updated flight search queries

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,12 +2,6 @@ class FlightsController < ApplicationController
   def index
     @airports = Airport.all
     @flights = Flight.all
-  end
-
-  private
-
-  def flight_params
-    params.require(:flight).require(:departure_airport, :arrival_airport,
-                                    :num_pax, :departure_time)
+    @results = Flight.search(params)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,27 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
+
+  def self.search(params)
+    return nil unless params[:flight].present?
+
+    params = params[:flight]
+
+    query = Flight.all
+
+    if params[:departure_airport].present? && params[:arrival_airport].present?
+      puts 'Found the params'
+
+      query = query.joins(:departure_airport,
+                          :arrival_airport)
+                   .where(airports: { code: params[:departure_airport] })
+                   .where(arrival_airport: { code: params[:arrival_airport] })
+
+    end
+
+    puts "Count is #{query.count}"
+    puts "Params provide #{params[:departure_airport]}"
+    puts "Query is #{query.inspect}"
+    query
+  end
 end
